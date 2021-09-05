bccbdb81bb30edb584d921f7be1dbed32f50c915
Justin Linn <justinslinn727@gmail.com>
2021-09-05 15:22:56 -0400

    Create Projects model via resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects
+  resources :blogs
+
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
-
-  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
