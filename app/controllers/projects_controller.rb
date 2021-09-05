0e6bed4a3f73b94b180ba1d099be47907564a4cd
Justin Linn <justinslinn727@gmail.com>
2021-09-05 19:30:10 -0400

    Integrated new controller for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,35 @@
 class ProjectsController < ApplicationController
+
+  # GET /project-items or /project-items.json
   def index 
     @project_items = Project.all
+  end
+
+  # GET /projects/1 or /projects/1.json
+  def show
+  end
+
+  # GET /projects/new
+  def new 
+    @project_item = Project.new
+  end
+
+  # GET /projects/1/edit
+  def edit
+  end
+
+  # POST /projects or /projects.json
+  def create
+    @project_item = Project.new(params.require(:project).permit(:title, :subtitle, :body))
+
+  respond_to do |format|
+      if @project_item.save
+        format.html { redirect_to projects_path, notice: "Project was successfully created. 🚀" }
+        format.json { render :show, status: :created, location: @project_item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @project_item.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
