ccc9906b817f7dd46a4efca2559b5882bdee35b8
Justin Linn <justinslinn727@gmail.com>
2021-09-05 19:59:34 -0400

    Add update controller and edit form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1/edit
   def edit
+    @project_item = Project.find(params[:id])
   end
 
   # POST /projects or /projects.json
@@ -30,6 +31,21 @@ class ProjectsController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @project_item.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  # PATCH/PUT /projects/1 or /projects/1.json
+  def update
+    @project_item = @project_item = Project.find(params[:id])
+
+    respond_to do |format|
+        if @project_item.update(params.require(:project).permit(:title, :subtitle, :body))
+          format.html { redirect_to projects_path, notice: "Project was successfully updated. 🎉" }
+          format.json { render :show, status: :ok, location: @project_item }
+        else 
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @project_item.errors, staus: :unprocessable_entity }
+        end
     end
   end
 end
