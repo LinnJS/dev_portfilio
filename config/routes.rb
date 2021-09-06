a6978072bc0a5362511a3e746f3f06689cf3c545
Justin Linn <justinslinn727@gmail.com>
2021-09-06 11:39:59 -0400

    Created toggle_status method on blog controller and intregrated in action link

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@
 Rails.application.routes.draw do
   resources :projects, except: [:show]
   resources :blogs
+
+  resources :blogs do 
+    member do
+      get :toggle_status
+    end
+  end
   
   get 'project/:id', to: 'projects#show', as: 'project_show'
   get 'about', to: 'pages#about'
