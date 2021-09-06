3ecd54379fe56cb50004717b9bf3cd6bfd8a7c00
Justin Linn <justinslinn727@gmail.com>
2021-09-05 21:27:12 -0400

    Add custom routes per project display page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  resources :projects
+  resources :projects, except: [:show]
   resources :blogs
-
+  
+  get 'project/:id', to: 'projects#show', as: 'project_show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
