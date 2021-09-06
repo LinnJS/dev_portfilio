2dfccb0a3b0be44e2d047cc75180842271e6f703
Justin Linn <justinslinn727@gmail.com>
2021-09-06 19:00:44 -0400

    Add project default images

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,11 @@ class Project < ApplicationRecord
   end
 
   scope :ruby_on_rails, -> { where(subtitle: 'The approach will not be easy. 1') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://images.unsplash.com/photo-1579935110378-81262796945f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+    self.thumb_image ||= "https://images.unsplash.com/photo-1620002093398-8f16081af5ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
+  end
 end
