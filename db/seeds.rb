5c8100c12315bdc47f34166ab28cb591e18c2188
Justin Linn <justinslinn727@gmail.com>
2021-09-13 20:08:38 -0400

    Create technology and relate it to projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,3 +55,11 @@ end
     thumb_image: 'https://images.unsplash.com/photo-1579935110378-81262796945f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
   )
 end
+
+puts "3 technologies created"
+
+3.times do |technology|
+  Project.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
