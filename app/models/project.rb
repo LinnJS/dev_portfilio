5d074fd3caf33130f87603f3ee7511d39141300c
Justin Linn <justinslinn727@gmail.com>
2021-09-13 20:45:26 -0400

    Add project defaults for images, and integreate nested attributes for portfilio model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies, 
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
   def self.react
@@ -8,4 +11,10 @@ class Project < ApplicationRecord
 
   scope :ruby_on_rails, -> { where(subtitle: 'The approach will not be easy. 1') }
 
+  after_initialize :set_defaults
+
+def set_defaults
+    self.main_image ||= "https://images.unsplash.com/photo-1620002093398-8f16081af5ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
+    self.thumb_image ||= "https://images.unsplash.com/photo-1579935110378-81262796945f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+end
 end
